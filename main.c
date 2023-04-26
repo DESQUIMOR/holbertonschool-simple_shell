4cd55e694819275d9c776422c9db737c968e6aa5
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-04-25 20:43:48 -0600

    shell.h, main.c

--- main.c
+++ main.c
@@ -1,134 +1,180 @@
 #include "shell.h"
 
+/**
+ * main - the main function
+ *
+ * Return: (Success) 0 always
+ * ------- (Fail) we drop out the looser :)
+ */
 int main(void)
 {
-    char *line = NULL;
-    char **args = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    int status;
+	sh_t data;
+	int pl;
 
-    while (1)
-    {
-        printf("$ ");
-
-        nread = getline(&line, &len, stdin);
-
-        if (nread == -1)
-        {
-            putchar('\n');
-            break;
-        }
-
-        line[nread - 1] = '\0';
-        args = split_line(line, " ");
-
-        if (args == NULL || args[0] == NULL)
-        {
-            free_args(args);
-            continue;
-        }
-
-        pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0)
-        {
-            if (execute(args) == -1)
-            {
-                free_args(args);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-
-        free_args(args);
-    }
-
-    free(line);
-
-    return 0;
+	_memset((void *)&data, 0, sizeof(data));
+	signal(SIGINT, signal_handler);
+	while (1)
+	{
+		index_cmd(&data);
+		if (read_line(&data) < 0)
+		{
+			if (isatty(STDIN_FILENO))
+				PRINT("\n");
+			break;
+		}
+		if (split_line(&data) < 0)
+		{
+			free_data(&data);
+			continue;
+		}
+		pl = parse_line(&data);
+		if (pl == 0)
+		{
+			free_data(&data);
+			continue;
+		}
+		if (pl < 0)
+		{
+			print_error(&data);
+			continue;
+		}
+		if (process_cmd(&data) < 0)
+		{
+			print_error(&data);
+			break;
+		}
+		free_data(&data);
+	}
+	free_data(&data);
+	exit(EXIT_SUCCESS);
 }
 
-char **split_line(char *line, const char *delim)
+/**
+ * read_line - read a line from the standard input
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int read_line(sh_t *data)
 {
-    char *token = NULL;
-    char **tokens = NULL;
-    size_t i, ntokens = 0;
+	char *csr_ptr, *end_ptr, c;
+	size_t size = BUFSIZE, read_st, length, new_size;
 
-    token = strtok(line, delim);
-
-    while (token != NULL)
-    {
-        tokens = realloc(tokens, sizeof(char *) * (ntokens + 1));
-
-        if (tokens == NULL)
-        {
-            perror("realloc");
-            exit(EXIT_FAILURE);
-        }
-
-        tokens[ntokens] = token;
-        ntokens++;
-
-        token = strtok(NULL, delim);
-    }
-
-    tokens = realloc(tokens, sizeof(char *) * (ntokens + 1));
-
-    if (tokens == NULL)
-    {
-        perror("realloc");
-        exit(EXIT_FAILURE);
-    }
-
-    tokens[ntokens] = NULL;
-
-    return tokens;
+	data->line = malloc(size * sizeof(char));
+	if (data->line == NULL)
+		return (FAIL);
+	if (isatty(STDIN_FILENO))
+		PRINT(PROMPT);
+	for (csr_ptr = data->line, end_ptr = data->line + size;;)
+	{
+		read_st = read(STDIN_FILENO, &c, 1);
+		if (read_st == 0)
+			return (FAIL);
+		*csr_ptr++ = c;
+		if (c == '\n')
+		{
+			*csr_ptr = '\0';
+			return (SUCCESS);
+		}
+		if (csr_ptr + 2 >= end_ptr)
+		{
+			new_size = size * 2;
+			length = csr_ptr - data->line;
+			data->line = _realloc(data->line, size * sizeof(char),
+						new_size * sizeof(char));
+			if (data->line == NULL)
+				return (FAIL);
+			size = new_size;
+			end_ptr = data->line + size;
+			csr_ptr = data->line + length;
+		}
+	}
 }
-
-void free_args(char **args)
+#define DELIMITER " \n\t\r\a\v"
+/**
+ * split_line - splits line to tokens
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int split_line(sh_t *data)
 {
-    size_t i;
+	char *token;
+	size_t size = TOKENSIZE, new_size, i = 0;
 
-    if (args == NULL)
-        return;
-
-    for (i = 0; args[i] != NULL; i++)
-        free(args[i]);
-
-    free(args);
+	if (_strcmp(data->line, "\n") == 0)
+		return (FAIL);
+	data->args = malloc(size * sizeof(char *));
+	if (data->args == NULL)
+		return (FAIL);
+	token = strtok(data->line, DELIMITER);
+	if (token == NULL)
+		return (FAIL);
+	while (token)
+	{
+		data->args[i++] =  token;
+		if (i + 2 >= size)
+		{
+			new_size = size * 2;
+			data->args = _realloc(data->args, size * sizeof(char *),
+					new_size * sizeof(char *));
+			if (data->args == NULL)
+				return (FAIL);
+			size = new_size;
+		}
+		token = strtok(NULL, DELIMITER);
+	}
+	data->args[i] = NULL;
+	return (0);
 }
-
-int execute(char **args)
+#undef DELIMITER
+#define DELIMITER ":"
+/**
+ * parse_line - parses arguments to valid command
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int parse_line(sh_t *data)
 {
-    char *buffer = NULL;
+	if (is_path_form(data) > 0)
+		return (SUCCESS);
+	if (is_builtin(data) > 0)
+	{
+		if (handle_builtin(data) < 0)
+			return (FAIL);
+		return (NEUTRAL);
+	}
+	is_short_form(data);
+	return (SUCCESS);
+}
+#undef DELIMITER
+/**
+ * process_cmd - process command and execute process
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int process_cmd(sh_t *data)
+{
+	pid_t pid;
+	int status;
 
-    buffer = malloc(sizeof(char) * 1024);
-
-    if (buffer == NULL)
-    {
-        perror("malloc");
-        return -1;
-    }
-
-    sprintf(buffer, "%s %s", args[0], args[1]);
-
-    if (execve("/bin/sh", (char *[]){"/bin/sh", "-c", buffer, NULL}, NULL) == -1)
-    {
-        perror("execve");
-        free(buffer);
-        return -1;
-    }
-
-    return 0;
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		if (execve(data->cmd, data->args, environ) < 0)
+		data->error_msg = _strdup("not found\n");
+			return (FAIL);
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
+	return (0);
 }
