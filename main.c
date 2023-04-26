965446fa708c3bd3593e8d75d2c9764af6327f58
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-04-25 19:40:32 -0600

    main

---
+++ main.c
@@ -0,0 +1,32 @@
+int main(void)
+{
+    char *command;
+    size_t bufsize = 0;
+
+    while (1) {
+        printf("$ ");
+        getline(&command, &bufsize, stdin);
+
+        if (feof(stdin)) {
+            break;
+        }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            if (execve(command, &command, NULL) == -1) {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            waitpid(pid, NULL, 0);
+        }
+
+        free(command);
+        command = NULL;
+    }
+
+    return 0;
+}
