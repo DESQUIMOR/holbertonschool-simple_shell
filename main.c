9b5eb05e9a00ba65d081d2fae61f3da546e4da45
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-04-25 20:01:13 -0600

    remake the main

--- main.c
+++ main.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(void)
-{
-    pid_t pid;
-    int status;
+#define MAX_COMMAND_LENGTH 100
 
-    printf("$ ");
-    pid = fork();
-    if (pid == -1)
+int main()
+{
+    char command[MAX_COMMAND_LENGTH];
+    int status = 1;
+
+    while(status)
     {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        /* Este es el proceso hijo */
-        char *argv[] = {"/bin/ls", NULL};
-        if (execve(argv[0], argv, NULL) == -1)
+        printf("Simple_Shell$ ");
+        fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+        // Remove the trailing newline character
+        strtok(command, "\n");
+
+        pid_t pid = fork();
+
+        if (pid < 0)
         {
-            perror("execve");
+            perror("fork");
             exit(EXIT_FAILURE);
         }
-    }
-    else
-    {
-        /* Este es el proceso padre */
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
+        else if (pid == 0)
         {
-            printf("Program exited with status %d\n", WEXITSTATUS(status));
+            char *args[] = {command, NULL};
+            if (execve(args[0], args, NULL) == -1)
+            {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            wait(&status);
         }
     }
+
     return EXIT_SUCCESS;
 }
