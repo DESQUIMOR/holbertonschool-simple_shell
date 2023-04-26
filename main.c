acdfa78969e0941ad52cf7c45f3ae5e7e672e119
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-04-25 20:38:27 -0600

    shell.h, main.c

--- main.c
+++ main.c
@@ -2,34 +2,133 @@
 
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
+    char *line = NULL;
+    char **args = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
 
     while (1)
     {
         printf("$ ");
-        if (getline(&buffer, &bufsize, stdin) == -1)
-            break;
 
-        if (fork() == 0)
+        nread = getline(&line, &len, stdin);
+
+        if (nread == -1)
         {
-            /* child process */
-            char **args = malloc(3 * sizeof(char *));
-            args[0] = "/bin/sh";
-            args[1] = "-c";
-            args[2] = buffer;
-            args[3] = NULL;
-            execve(args[0], args, NULL);
-            perror("execve failed");
+            putchar('\n');
+            break;
+        }
+
+        line[nread - 1] = '\0';
+        args = split_line(line, " ");
+
+        if (args == NULL || args[0] == NULL)
+        {
+            free_args(args);
+            continue;
+        }
+
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
             exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            if (execute(args) == -1)
+            {
+                free_args(args);
+                exit(EXIT_FAILURE);
+            }
         }
         else
         {
-            /* parent process */
-            wait(NULL);
+            wait(&status);
         }
+
+        free_args(args);
     }
 
-    free(buffer);
-    return EXIT_SUCCESS;
+    free(line);
+
+    return 0;
+}
+
+char **split_line(char *line, const char *delim)
+{
+    char *token = NULL;
+    char **tokens = NULL;
+    size_t i, ntokens = 0;
+
+    token = strtok(line, delim);
+
+    while (token != NULL)
+    {
+        tokens = realloc(tokens, sizeof(char *) * (ntokens + 1));
+
+        if (tokens == NULL)
+        {
+            perror("realloc");
+            exit(EXIT_FAILURE);
+        }
+
+        tokens[ntokens] = token;
+        ntokens++;
+
+        token = strtok(NULL, delim);
+    }
+
+    tokens = realloc(tokens, sizeof(char *) * (ntokens + 1));
+
+    if (tokens == NULL)
+    {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+
+    tokens[ntokens] = NULL;
+
+    return tokens;
+}
+
+void free_args(char **args)
+{
+    size_t i;
+
+    if (args == NULL)
+        return;
+
+    for (i = 0; args[i] != NULL; i++)
+        free(args[i]);
+
+    free(args);
+}
+
+int execute(char **args)
+{
+    char *buffer = NULL;
+
+    buffer = malloc(sizeof(char) * 1024);
+
+    if (buffer == NULL)
+    {
+        perror("malloc");
+        return -1;
+    }
+
+    sprintf(buffer, "%s %s", args[0], args[1]);
+
+    if (execve("/bin/sh", (char *[]){"/bin/sh", "-c", buffer, NULL}, NULL) == -1)
+    {
+        perror("execve");
+        free(buffer);
+        return -1;
+    }
+
+    return 0;
 }
--- shell.h
+++ shell.h
@@ -1,19 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* Libraries */
+#include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
 
-/* Global Variables */
-extern char **environ;
-
-/* Functions */
-char *_strdup(char *str);
-char **_splitline(char *line, const char *delim);
-int _execute(char **args);
+/* Function prototypes */
+char **split_line(char *line, const char *delim);
+void free_args(char **args);
+int execute(char **args);
 
 #endif /* SHELL_H */
