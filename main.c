78b0da729044acaa358219e13277fa6330151e21
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-04-25 20:19:56 -0600

    main

--- main.c
+++ main.c
@@ -1,34 +1,28 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 int main(void)
 {
-    char *args[] = {"/bin/ls", "-l", "/usr/", NULL};
-    char buffer[100];
+    pid_t pid;
 
-    pid_t pid = fork();
-
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Error:");
+        exit(EXIT_FAILURE);
+    }
     if (pid == 0)
     {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror("Error executing command");
-            exit(EXIT_FAILURE);
-        }
+        execve("/bin/ls", NULL, NULL);
+        perror("Error:");
+        exit(EXIT_FAILURE);
     }
-    else if (pid > 0)
+    else
     {
         wait(NULL);
         printf("Child exited\n");
     }
-    else
-    {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
-
-    return 0;
+    return (EXIT_SUCCESS);
 }
